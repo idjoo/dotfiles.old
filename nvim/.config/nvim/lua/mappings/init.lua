361b3e671381c388004e265bcd97e6789a05fd6f
Vian <cocatrip@proton.me>
2022-07-21 18:26:28 +0700

    change mapping to more verbose using <cmd>

--- nvim/.config/nvim/lua/mappings/init.lua
+++ nvim/.config/nvim/lua/mappings/init.lua
@@ -1,23 +1,20 @@
--- Formatter
-vim.api.nvim_set_keymap("n", "<leader>bf", "<cmd>Format<cr>", { silent = true })
-
 -- Telescope
-vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
 
 -- Bufferline
-vim.api.nvim_set_keymap("n", "[b", ":BufferLineCyclePrev<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap("n", "]b", ":BufferLineCycleNext<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "]b", "<cmd>BufferLineCycleNext<cr>", { silent = true, noremap = true })
 
 -- Bufdelete
-vim.api.nvim_set_keymap("n", "<leader>bd", ":Bdelete<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>bd", "<cmd>Bdelete<cr>", { silent = true, noremap = true })
 
 -- vim-fugitive
-vim.api.nvim_set_keymap("n", "<leader>gs", ":G<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>gh", ":diffget //3<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>gf", ":diffget //2<cr>", {})
+vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>G<cr>", {})
+vim.api.nvim_set_keymap("n", "<leader>gh", "<cmd>diffget //3<cr>", {})
+vim.api.nvim_set_keymap("n", "<leader>gf", "<cmd>diffget //2<cr>", {})
 
 -- glow
-vim.api.nvim_set_keymap("n", "<leader>mp", ":Glow<cr>", {})
+vim.api.nvim_set_keymap("n", "<leader>mp", "<cmd>Glow<cr>", {})
