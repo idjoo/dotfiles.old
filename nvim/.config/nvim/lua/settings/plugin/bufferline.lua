08ad860f3729692abdd8b957fc0d8f253ec64ee5
Vian <cocatrip@proton.me>
2022-10-16 00:37:50 +0700

    fix: nvim - deprecated bufferline config move diagnostics_indicator to options

--- nvim/.config/nvim/lua/settings/plugin/bufferline.lua
+++ nvim/.config/nvim/lua/settings/plugin/bufferline.lua
@@ -1,6 +1,8 @@
 require('bufferline').setup {
-  diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    local icon = level:match("error") and " " or " "
-    return " " .. icon .. count
-  end
+  options = {
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end
+  }
 }
