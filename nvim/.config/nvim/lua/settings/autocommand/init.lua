bb46a55f2f63e9e4a02b4819f53d9659353be8c5
Vian <cocatrip@proton.me>
2022-10-23 16:01:42 +0700

    feat: nvim - make a new autocommand for mdx file to set filetype to md

--- nvim/.config/nvim/lua/settings/autocommand/init.lua
+++ nvim/.config/nvim/lua/settings/autocommand/init.lua
@@ -2,7 +2,7 @@ vim.cmd([[
 autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif 
 ]])
 
-vim.api.nvim_create_autocmd('BufRead,BufNewFile', {
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = '*.mdx',
   command = 'set filetype=markdown'
 })
